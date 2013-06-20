e547bad8f83b8b2ba199f8ba1da027f474c4a13e
Andrew Peterson <andy@ndpsoftware.com>
2013-06-20 07:12:13 -0700

    Add dependent :destroy for has_many relationships to make cleanup easier.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,24 +3,15 @@ class Project
   include Mongoid::Timestamps
   field :name, type: String
   field :valid_estimates, type: Array
-  has_many :stories
-  has_many :milestones
+  has_many :stories, dependent: :destroy
+  has_many :milestones, dependent: :destroy
   embeds_many :workers
+
   embeds_one :workflow
   field :past_states, type: Array
   field :present_states, type: Array
   field :future_states, type: Array
 
-  def self.seed
-    #Project.delete_all
-    Project.create! name: 'Sample Project Again',
-                    states: %w{unscheduled unstarted in_progress delivered finished accepted},
-                    past_states: %w{accepted},
-                    present_states: %w{in_progress delivered finished},
-                    future_states: %w{unstarted},
-                    valid_estimates: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-  end
 
   def map_state_to_epoch(state)
     return 'future' if future_states.include? state
