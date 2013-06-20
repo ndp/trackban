6b4b222428364bb9bbeac1bc039bd9c1a17f40d4
Andrew Peterson <andy@ndpsoftware.com>
2013-06-20 07:12:38 -0700

    Add Zurb foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
+//= require csster
 //= require_tree ./angularjs/
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
