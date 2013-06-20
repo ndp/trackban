6cbed072c83f1dddb982f032480d57dfa466b68a
Andrew Peterson <andy@ndpsoftware.com>
2013-06-19 20:32:03 -0700

    add results of angular_scaffold plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Trackban::Application.routes.draw do
 
+  resources :posts
+
+
+  get "welcome/index"
+
   get "stories/index"
 
   get "stories/show"
@@ -75,7 +80,7 @@ Trackban::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
